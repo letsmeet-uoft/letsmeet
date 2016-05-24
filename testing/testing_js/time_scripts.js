06b545244509e3973f551600a66272466aa18590
fyyaz <fyyazk@gmail.com>
2016-05-24 15:10:23 -0400

    added js to seperate file and fixed time conversion# Please enter the commit message for your changes. Lines starting

--- testing/testing_js/time_scripts.js
+++ testing/testing_js/time_scripts.js
@@ -1,5 +1,56 @@
 var tz1 = 0; 
 var tz2 = 0;
+
+function convertTimeZone2to1(valueTimeZone1, valueTimeZone2, name1, name2) {
+	// if timezone1 is -4 and timezone2 is -11
+// if (valueTimeZone1 === undefined || valueTimeZone2 === undefined){
+// 	return 0;
+// }
+	valueTimeZone1 = valueTimeZone1/10;
+	if (valueTimeZone1 >= 0){
+		valueTimeZone1 = Math.floor(valueTimeZone1);
+	}
+	else{
+		valueTimeZone1 = Math.ceil(valueTimeZone1);	
+	}
+	valueTimeZone2 = valueTimeZone2/10;
+	if (valueTimeZone2 >= 0){
+		valueTimeZone2 = Math.floor(valueTimeZone2);
+	}
+	else{
+		valueTimeZone2 = Math.ceil(valueTimeZone2);	
+	}
+	var diff = valueTimeZone1 - valueTimeZone2;
+	//window.alert(getTimeuserDef(valueTimeZone1 , diff));
+	// window.alert(valueTimeZone1);
+	// window.alert(valueTimeZone2);
+	// window.alert(diff);
+	
+	var text = " ";
+	var i;
+	for (i = 0 ; i < 24 ; i++){
+		var hold = getTimeuserDef(i , diff);
+		var yt = new Date();
+		//window.alert(yt);
+		hold = hold - yt+1000;
+		var q = new Date();
+		q.setTime(hold);
+		var final = q.getHours();
+		if (final >= 24){
+			final=0;
+		}
+
+		if (i > 8 && i < 18 && final > 8 && final < 18){
+			console.log(i +" <---MATCH----> "+ final);			
+		}
+		else{
+		console.log(i +" <------------> "+ final);
+	}
+	}
+	return 0;
+}
+
+
 function myFunction(value, useDaylightTime) {
 	var y = new Date();
 	var n = y.getTimezoneOffset();
